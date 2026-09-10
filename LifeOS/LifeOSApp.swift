cf4f9760c4e7164c2d1601560a60e27db8084c60
Pamela <pamela.lotriet@gmail.com>
2026-09-10 10:49:05 +0200

    Initial Commit

---
+++ LifeOS/LifeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeOSApp.swift
+//  LifeOS
+//
+//  Created by RetroRabbit on 2026/09/10.
+//
+
+import SwiftUI
+
+@main
+struct LifeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
