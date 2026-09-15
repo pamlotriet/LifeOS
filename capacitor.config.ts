0bc61dd80289a4c376297870d2c7b9bf40e52a54
pamela lotriet <pamelalotriet@gmail.com>
2026-09-15 16:46:20 +0200

    Added capacitor to the project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pamelalotriet.lifeos',
+  appName: 'lifeos',
+  webDir: 'dist'
+};
+
+export default config;
